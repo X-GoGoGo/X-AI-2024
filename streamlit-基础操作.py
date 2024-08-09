b078190bc0ddd7315712f887d988569be74cadee
X-GoGoGo <xueshuwen813@163.com>
2024-08-09 11:52:39 +0800

    Add files via upload

--- streamlit-基础操作.py
+++ streamlit-基础操作.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.title("这是一个网站标题") #创建网站标题
 st.write("## Hello,Streamlit") #创建一个文本
 "你好"  #创建一个文本
-# st.image('C:\phthon-相关\pythonProject2\.venv\标签-火热.png',width=50) #创建图片
+st.image('https://raw.githubusercontent.com/X-GoGoGo/X-AI-2024/main/%E6%A0%87%E7%AD%BE-%E7%81%AB%E7%83%AD.png',width=50) #创建图片
 st.divider() #创建分割线
 df = pd.DataFrame(  #创建一个dataframe
     {
